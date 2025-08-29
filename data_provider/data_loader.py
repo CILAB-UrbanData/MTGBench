900fc5342e05da8162d669bbac2337d5a355f921
light <166206742+ray-oflight@users.noreply.github.com>
2025-08-29 18:08:31 +0800

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -423,7 +423,7 @@ class SF20_forTrGNN_Dataset(Dataset):
 
 
 
-class DiffTrajProcess:
+class ChengduForDifftraj:
     def __init__(self, config: dict):
         required_keys = ["traj_length", "grid_size", "input_csv", "output_dir", "min_points"]
         for key in required_keys:
